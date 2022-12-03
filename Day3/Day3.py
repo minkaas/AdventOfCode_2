ddf0e01fda1946468e19144dd55cc741fb6e5b65
s2457512 <m.w.bohlmeijer@student.utwente.nl>
2022-12-03 06:33:33 +0100

    Day 3 done :sleeping:

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,26 +1,51 @@
 import pathlib
+import string
 
 
 def parse(puzzle_input):
     values = puzzle_input.split("\n")
     data = []
+    result = []
+    i = 0
     for value in values:
-        data.append(int(value))
-    return data
+        result.append(value)
+
+    for value in values:
+        firstpart, secondpart = value[:len(value) // 2], value[len(value) // 2:]
+        data.append([firstpart, secondpart])
+    return data, result
 
 
 def part1(data):
-    return 0
+    priority = 0
+    for rucksacks in data:
+        rs, comp = set(rucksacks[0]), set(rucksacks[1])
+        item = rs.intersection(comp).pop()
+        priority += item_to_index(item)
+    return priority
+
+
+def item_to_index(item):
+    if item.islower():
+        return string.ascii_lowercase.index(item) + 1
+    return string.ascii_uppercase.index(item) + 27
 
 
 def part2(data):
-    return 0
+    i = 0
+    priority = 0
+    while i < len(data):
+        elf1, elf2, elf3 = set(data[i]), set(data[i + 1]), set(data[i + 2])
+        item = elf1.intersection(elf2, elf3).pop()
+        priority += item_to_index(item)
+        i += 3
+    return priority
 
 
 def solve(puzzle_input):
-    data = parse(puzzle_input)
+    data, data2 = parse(puzzle_input)
     sol1 = part1(data)
-    sol2 = part2(data)
+    sol2 = part2(data2)
     return sol1, sol2
 
 
