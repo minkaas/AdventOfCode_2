7ccd72ac4c1042f6ff5702e0ea53efed12ff5206
s2457512 <m.w.bohlmeijer@student.utwente.nl>
2022-12-04 17:01:42 +0100

    Did day 4 in R like a real programmer :sunglasses:

---
+++ Day4/Day4.R
@@ -0,0 +1,24 @@
+input <- readLines("input")
+one <- 0
+two <- 0
+three <- 0
+four <- 0
+fullycontained <- 0
+partlycontained <- 0
+for (assignments in input) {
+  asses <- strsplit(assignments, ",")[[1]]
+  ass <- strsplit(asses[1], "-")[[1]]
+  one <- as.numeric(ass[1])
+  two <- as.numeric(ass[2])
+  ign <- strsplit(asses[2], "-")[[1]]
+  three <- as.numeric(ign[1])
+  four <- as.numeric(ign[2])
+  if ((one >= three && four >= two) || (one <= three && four <= two)) {
+    fullycontained <- fullycontained + 1
+  }
+  if ((max(one, three) <= min(two, four))) {
+    partlycontained <- partlycontained + 1
+  } 
+}
+print(fullycontained)
+print(partlycontained)
