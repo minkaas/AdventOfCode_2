512bde3de99f5bb6ed1d369375ddb267b772e585
s2457512 <m.w.bohlmeijer@student.utwente.nl>
2022-12-07 12:03:03 +0100

    Ouch

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -3,29 +3,79 @@ import pathlib
 
 def parse(puzzle_input):
     values = puzzle_input.split("\n")
-    data = []
+    directories = {}
+    files = {}
+    topdir = {}
+    currentdir = ""
     for value in values:
-        data.append(int(value))
-    return data
+        if "$ cd" in value:
+            directory = value.split(" ")
+            newdir = currentdir + "/" + directory[2]
+            if directory[2] != ".." and newdir not in directories:
+                directories[newdir] = []
+                files[newdir] = []
+                topdir[newdir] = currentdir
+                currentdir = newdir
+            elif directory[2] == "..":
+                currentdir = topdir[currentdir]
+            else:
+                currentdir = currentdir + "/" + directory[1]
+        elif "dir" in value:
+            directory = value.split(" ")
+            directories[currentdir].append(currentdir + "/" + directory[1])
+        elif value.split(" ")[0].isdigit():
+            filesize = value.split(" ")
+            files[currentdir].append(int(filesize[0]))
+    return directories, files
 
 
-def part1(data):
-    return 0
+def getdirvalue(dir, depth, files, result):
+    if len(depth[dir]) != 0:
+        result[dir] = 0
+        for directories in depth[dir]:
+            result = getdirvalue(directories, depth, files, result)
+        for directories in depth[dir]:
+            result[dir] += result[directories]
+        for file in files[dir]:
+            result[dir] += file
+    else:
+        result[dir] = 0
+        for file in files[dir]:
+            result[dir] += file
+    return result
 
 
-def part2(data):
-    return 0
+def part1(depth, files):
+    directorysizes = getdirvalue('//', depth, files, {})
+    sum = 0
+    for dirs in directorysizes:
+        if directorysizes[dirs] <= 100000:
+            sum += directorysizes[dirs]
+    return sum
+
+
+def part2(depth, files):
+    directorysizes = getdirvalue('//', depth, files, {})
+    sum = 0
+    for dirs in directorysizes:
+        sum += directorysizes[dirs]
+    to_be_freed = directorysizes['//'] - 40000000
+    possibledirs = []
+    for dirs in directorysizes:
+        if directorysizes[dirs] >= to_be_freed:
+            possibledirs.append(directorysizes[dirs])
+    return min(possibledirs)
 
 
 def solve(puzzle_input):
-    data = parse(puzzle_input)
-    sol1 = part1(data)
-    sol2 = part2(data)
+    depth, files = parse(puzzle_input)
+    sol1 = part1(depth, files)
+    sol2 = part2(depth, files)
     return sol1, sol2
 
 
 def run():
-    puzzle_input = pathlib.Path("input").read_text().strip()
+    puzzle_input = pathlib.Path("input.txt").read_text().strip()
     solutions = solve(puzzle_input)
     print(solutions)
 
