6eadc69ad3a10a44131aa79e80137baa3da21f0b
minkaas <m.w.bohlmeijer@student.utwente.nl>
2023-12-09 06:32:33 +0100

    This was strangely easy :smiling_face_with_three_hearts:

--- 2023/Day9/Day9.py
+++ 2023/Day9/Day9.py
@@ -5,17 +5,48 @@ def parse(puzzle_input):
     values = puzzle_input.split("\n")
     data = []
     for value in values:
-        data.append(value)
+        res = []
+        for num in value.split(" "):
+            res.append(int(num))
+        data.append(res)
     return data
+
+
+def get_history(history):
+    result = 0
+    new_history = []
+    for i in range(0, len(history) - 1):
+        one, two = history[i], history[i + 1]
+        new_history.append(two - one)
+    if any(new_history):
+        result += get_history(new_history)
+    result += history[-1]
+    return result
 
 
 def part1(data):
     result = 0
+    for history in data:
+        result += get_history(history)
     return result
 
 
 def part2(data):
     result = 0
+    for history in data:
+        result += get_backwards_history(history)
+    return result
+
+
+def get_backwards_history(history):
+    result = 0
+    new_history = []
+    for i in range(0, len(history) - 1):
+        one, two = history[i], history[i + 1]
+        new_history.append(two - one)
+    if any(new_history):
+        result += get_backwards_history(new_history)
+    result = history[0] - result
     return result
 
 
