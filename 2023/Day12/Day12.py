5413b14866c70f861ca63037b6c8c33e774d0a8e
minkaas <m.w.bohlmeijer@student.utwente.nl>
2023-12-12 09:28:38 +0100

    Did part 1, don't even want to think about part 2 :sob:

--- 2023/Day12/Day12.py
+++ 2023/Day12/Day12.py
@@ -1,29 +1,76 @@
+import functools
+import math
 import pathlib
 from time import time
 
 
 def parse(puzzle_input):
     values = puzzle_input.split("\n")
-    data = []
+    springs = []
+    condition = []
     for value in values:
-        data.append(value)
-    return data
+        value = value.split(" ")
+        springs.append(value[0])
+        cond = []
+        for val in value[1].split(","):
+            cond.append(int(val))
+        condition.append(cond)
+    return springs, condition
 
 
-def part1(data):
+def count_matches(spring, spr_size, crit):
+    if len(crit) == 0:
+        # criteria is good, check if there are no more #s
+        if not any(value == '#' for value in spring):
+            return 1
+        else:
+            return 0
+    # get first criteria and the other stuff
+    head = crit[0]
+    tail = crit[1:]
+    space_needed = sum(tail) + len(tail)
     result = 0
+    for before in range(spr_size-space_needed-head+1):
+        cand = '.' * before + '#' * head + '.'
+        zipped = zip(spring, cand)
+        can_do = True
+        for c0, c1 in zipped:
+            if not (c0 == c1 or c0=='?'):
+                can_do = False
+                break
+        if can_do:
+            result += count_matches(spring[len(cand):], spr_size-head-before-1, tail)
     return result
 
 
-def part2(data):
+def part1(springs, condition):
     result = 0
+    for i in range(len(springs)):
+        spring, cond = springs[i], condition[i]
+        result += count_matches(spring, len(spring), cond)
+    return result
+
+
+def unfold(spring, cond):
+    cond = cond * 5
+    spring_result = spring
+    for i in range(4):
+        spring_result = spring_result + '?' + spring
+    return spring_result, cond
+
+
+def part2(springs, condition):
+    result = 0
+    for i in range(len(springs)):
+        spring, cond = unfold(springs[i], condition[i])
+        result += count_matches(spring, len(spring), cond)
     return result
 
 
 def solve(puzzle_input):
-    data = parse(puzzle_input)
-    sol1 = part1(data)
-    sol2 = part2(data)
+    springs, condition = parse(puzzle_input)
+    sol1 = part1(springs, condition)
+    sol2 = part2(springs, condition)
     return sol1, sol2
 
 
