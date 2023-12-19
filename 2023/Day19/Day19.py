68502aa36df8c0be62f9b82032566203080e18d8
minkaas <m.w.bohlmeijer@student.utwente.nl>
2023-12-19 05:59:24 +0100

    This better be a 20 minute part 1 kind of day

---
+++ 2023/Day19/Day19.py
@@ -0,0 +1,37 @@
+import pathlib
+from time import time
+
+
+def parse(puzzle_input):
+    values = puzzle_input.split("\n")
+    data = []
+    for value in values:
+        data.append(value)
+    return data
+
+
+def part1(data):
+    result = 0
+    return result
+
+
+def part2(data):
+    result = 0
+    return result
+
+
+def solve(puzzle_input):
+    data = parse(puzzle_input)
+    sol1 = part1(data)
+    sol2 = part2(data)
+    return sol1, sol2
+
+
+def run():
+    start_time = time()
+    puzzle_input = pathlib.Path("input").read_text().strip()
+    solutions = solve(puzzle_input)
+    print(solutions)
+    print("This took ", time() - start_time)
+
+run()
