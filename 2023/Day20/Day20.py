6e907065facfe9f49a171a17a1a75b1686f9d59b
minkaas <m.w.bohlmeijer@student.utwente.nl>
2023-12-20 06:14:18 +0100

    I don't understand shit about today, this is for some other time :whale2:

--- 2023/Day20/Day20.py
+++ 2023/Day20/Day20.py
@@ -4,32 +4,43 @@ from time import time
 
 def parse(puzzle_input):
     values = puzzle_input.split("\n")
-    data = []
+    flipflops = {}
+    conjunctions = {}
+    broadcast = []
     for value in values:
-        data.append(value)
-    return data
+        value = value.split(" -> ")
+        if value[0] == "broadcaster":
+            broadcast = value[1].split(", ")
+        elif value[0][0] == "%":
+            flipflops[value[0][1:]] = value[1].split(", ")
+        elif value[0][0] == "&":
+            conjunctions[value[0][1:]] = value[1].split(", ")
+    return flipflops, conjunctions, broadcast
 
 
-def part1(data):
+def part1(flipflops, conjunctions, broadcast):
+    print(flipflops)
+    print(conjunctions)
+    print(broadcast)
     result = 0
     return result
 
 
-def part2(data):
+def part2(flipflops, conjunctions, broadcast):
     result = 0
     return result
 
 
 def solve(puzzle_input):
-    data = parse(puzzle_input)
-    sol1 = part1(data)
-    sol2 = part2(data)
+    flipflops, conjunctions, broadcast = parse(puzzle_input)
+    sol1 = part1(flipflops, conjunctions, broadcast)
+    sol2 = part2(flipflops, conjunctions, broadcast)
     return sol1, sol2
 
 
 def run():
     start_time = time()
-    puzzle_input = pathlib.Path("input").read_text().strip()
+    puzzle_input = pathlib.Path("test").read_text().strip()
     solutions = solve(puzzle_input)
     print(solutions)
     print("This took ", time() - start_time)
