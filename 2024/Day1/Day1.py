b342485c755e5b3b47ba0e5376580dbe1c66675b
Bohlmeijer, M.W. (Minke, Student M-CS) <m.w.bohlmeijer@student.utwente.nl>
2024-12-01 06:12:41 +0100

    could have been better :pensive: :sleeping:

--- 2024/Day1/Day1.py
+++ 2024/Day1/Day1.py
@@ -4,32 +4,41 @@ from time import time
 
 def parse(puzzle_input):
     values = puzzle_input.split("\n")
-    data = []
+    left_list = []
+    right_list = []
     for value in values:
-        data.append(value)
-    return data
+        lr = value.split()
+        left_list.append(int(lr[0]))
+        right_list.append(int(lr[1]))
+    return left_list, right_list
 
 
-def part1(data):
+def part1(left_list, right_list):
+    left_list.sort()
+    right_list.sort()
     result = 0
+    for i in range(0, len(left_list)):
+        result += abs(left_list[i] - right_list[i])
     return result
 
 
-def part2(data):
+def part2(left_list, right_list):
     result = 0
+    for i in left_list:
+        result += i * right_list.count(i)
     return result
 
 
 def solve(puzzle_input):
-    data = parse(puzzle_input)
-    sol1 = part1(data)
-    sol2 = part2(data)
+    left_list, right_list = parse(puzzle_input)
+    sol1 = part1(left_list, right_list)
+    sol2 = part2(left_list, right_list)
     return sol1, sol2
 
 
 def run():
     start_time = time()
-    puzzle_input = pathlib.Path("severity").read_text().strip()
+    puzzle_input = pathlib.Path("input").read_text().strip()
     solutions = solve(puzzle_input)
     print(solutions)
     print("This took ", time() - start_time)
