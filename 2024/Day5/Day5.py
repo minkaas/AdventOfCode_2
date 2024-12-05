ccc30e231577684fa54d31857ab911ce25623f4f
Bohlmeijer, M.W. (Minke, Student M-CS) <m.w.bohlmeijer@student.utwente.nl>
2024-12-05 06:28:04 +0100

    my original code did not have the while loop for part 2 :grimacing:

--- 2024/Day5/Day5.py
+++ 2024/Day5/Day5.py
@@ -3,33 +3,71 @@ from time import time
 
 
 def parse(puzzle_input):
-    values = puzzle_input.split("\n")
-    data = []
-    for value in values:
-        data.append(value)
-    return data
+    values = puzzle_input.split("\n\n")
+    updates = []
+    order_rules = []
+    for value in values[0].split("\n"):
+        left, right = value.split("|")
+        order_rules.append((int(left), int(right)))
+    for value in values[1].split("\n"):
+        ups = value.split(",")
+        ups = [int(x) for x in ups]
+        updates.append(ups)
+    return updates, order_rules
 
 
-def part1(data):
+def part1(updates, order_rules):
     result = 0
+    for up in updates:
+        if not breaks_rule(up, order_rules):
+            result += up[len(up)//2]
     return result
 
 
-def part2(data):
+def breaks_rule(update, order_rules):
+    for rule in order_rules:
+        if rule[0] in update and rule[1] in update:
+            if not update.index(rule[0]) < update.index(rule[1]):
+                return True
+    return False
+
+
+def part2(updates, order_rules):
     result = 0
+    for up in updates:
+        if breaks_rule(up, order_rules):
+            new_up = change_order(up, order_rules)
+            result += new_up[len(new_up)//2]
     return result
+
+
+def change_order(update, order_rules):
+    prev = 0
+    changes = True
+    while changes:
+        for rule in order_rules:
+            if rule[0] in update and rule[1] in update:
+                i_0 = update.index(rule[0])
+                i_1 = update.index(rule[1])
+                if not i_0 < i_1:
+                    update[i_0], update[i_1] = update[i_1], update[i_0]
+                    prev = 1
+        if prev == 0:
+            changes = False
+        prev = 0
+    return update
 
 
 def solve(puzzle_input):
-    data = parse(puzzle_input)
-    sol1 = part1(data)
-    sol2 = part2(data)
+    updates, order_rules = parse(puzzle_input)
+    sol1 = part1(updates, order_rules)
+    sol2 = part2(updates, order_rules)
     return sol1, sol2
 
 
 def run():
     start_time = time()
-    puzzle_input = pathlib.Path("input").read_text().strip()
+    puzzle_input = pathlib.Path("test").read_text().strip()
     solutions = solve(puzzle_input)
     print(solutions)
     print("This took ", time() - start_time)
