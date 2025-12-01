01fcd80feb9f086b81aad761552dc193d9087419
Bohlmeijer, M.W. (Minke, Student M-CS) <m.w.bohlmeijer@student.utwente.nl>
2025-12-01 06:14:17 +0100

    grrr modulo 💢

--- 2025/Day1/Day1.py
+++ 2025/Day1/Day1.py
@@ -6,18 +6,44 @@ def parse(puzzle_input):
     values = puzzle_input.split("\n")
     data = []
     for value in values:
-        data.append(int(value))
+        data.append(value)
     return data
 
 
 def part1(data):
-    result = 0
-    return result
+    result = 50
+    final = 0
+    for point in data:
+        if 'R' in point:
+            result += int(point[1:])
+            result %= 100
+        else:
+            result -= int(point[1:])
+            result += 100
+            result %= 100
+        if result == 0:
+            final += 1
+    return final
 
 
 def part2(data):
-    result = 0
-    return result
+    result = 50
+    final = 0
+    for point in data:
+        if 'R' in point:
+            for i in range(0, int(point[1:])):
+                result += 1
+                result %= 100
+                if result == 0:
+                    final += 1
+        else:
+            for i in range(0, int(point[1:])):
+                result -= 1
+                result += 100
+                result %= 100
+                if result == 0:
+                    final += 1
+    return final
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
