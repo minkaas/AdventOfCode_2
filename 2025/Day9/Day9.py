22e73c49157e93daca5ae9f686c035dc7a238141
Bohlmeijer, M.W. (Minke, Student M-CS) <m.w.bohlmeijer@student.utwente.nl>
2025-12-09 07:52:25 +0100

    part 2 that's a bit more me

--- 2025/Day9/Day9.py
+++ 2025/Day9/Day9.py
@@ -25,54 +25,33 @@ def part1(data):
 
 def part2(data):
     result = 0
-    data.append(data[0])
-    boundaries = {}
-    for i in range(0, len(data) - 1):
-        tile1 = data[i]
-        tile2 = data[i + 1]
-        x1, y1 = tile1
-        x2, y2 = tile2
-        if x1 == x2:
-            for y in range(min(y1,y2), max(y1, y2) + 1):
-                if y not in boundaries:
-                    boundaries[y] = [x1, x1]
-                else:
-                    boundaries[y][0] = min(boundaries[y][0], x1)
-                    boundaries[y][1] = max(boundaries[y][1], x1)
-        elif y1 == y2:
-            if y1 not in boundaries:
-                boundaries[y1] = [min(x1, x2), max(x1, x2)]
-            else:
-                boundaries[y1][0] = min(boundaries[y1][0], x1, x2)
-                boundaries[y1][1] = max(boundaries[y1][1], x1, x2)
     for i in range(0, len(data)):
         for j in range(i + 1, len(data)):
-            valid = True
-            tile1 = data[i]
-            tile2 = data[j]
-            x1, y1 = tile1
-            x2, y2 = tile2
-            minx = min(x1, x2)
-            maxx = max(x1, x2)
-            miny = min(y1, y2)
-            maxy = max(y1, y2)
-            for y in range(miny, maxy + 1):
-                if y not in boundaries:
-                    valid = False
-                    break
-                boundminx, boundmaxx = boundaries[y]
-                if minx < boundminx or maxx > boundmaxx:
-                    valid = False
-                    break
-            if valid:
-                result = max(result, rectangle_size(tile1, tile2))
+            if check_valid(data, data[i], data[j]):
+                result = max(result, rectangle_size(data[i], data[j]))
     return result
+
+
+def check_valid(data, tile1, tile2):
+    # if the rectangle is intersected by a square it is extremely cap
+    minx = min(tile1[0], tile2[0])
+    maxx = max(tile1[0], tile2[0])
+    miny = min(tile1[1], tile2[1])
+    maxy = max(tile1[1], tile2[1])
+
+    for i in range(0, len(data)):
+        lineseg1 = data[i]
+        lineseg2 = data[(i + 1) % len(data)]
+        if not (max(lineseg1[0], lineseg2[0]) <= minx or maxx <= min(lineseg1[0], lineseg2[0]) or max(lineseg1[1], lineseg2[1]) <= miny or maxy <= min(lineseg1[1], lineseg2[1])):
+            return False
+    return True
+
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
     sol1 = part1(data)
     data = parse(puzzle_input)
-    sol2 = part2(data)
+    sol2 = part22(data)
     return sol1, sol2
 
 
