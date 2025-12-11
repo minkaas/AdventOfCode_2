f16e9f4d93ba9b8ed454f9426d04296652be8cab
Bohlmeijer, M.W. (Minke, Student M-CS) <m.w.bohlmeijer@student.utwente.nl>
2025-12-11 07:36:17 +0100

    it was pathfinding, but a casual 350 seconds is not enough to scare me

--- 2025/Day11/Day11.py
+++ 2025/Day11/Day11.py
@@ -1,22 +1,66 @@
 import pathlib
 from time import time
+from types import new_class
 
 
 def parse(puzzle_input):
     values = puzzle_input.split("\n")
-    data = []
+    data = {}
     for value in values:
-        data.append(int(value))
+        devices, outputs = value.split(":")
+        data[devices] = outputs.split()
     return data
 
 
-def part1(data):
+def find_all_paths(data, path, start, end):
+    path.append(start)
     result = 0
+    if start == end:
+        return 1
+    else:
+        for i in data[start]:
+            result += find_all_paths(data, path, i, end)
     return result
 
 
-def part2(data):
+def part1(data):
+    result = find_all_paths(data, [], "you", "out")
+    return result
+
+
+def find_all_paths_visited(data, path, start, end, visited):
+    path.append(start)
+    visited.add(start)
     result = 0
+    if start == end:
+        result += 1
+    else:
+        if start not in data:
+            path.pop()
+            return 0, visited
+        for i in data[start]:
+            result += find_all_paths_visited(data, path, i, end, visited)[0]
+    path.pop()
+    return result, visited
+
+
+def part2(data):
+    result, visited = find_all_paths_visited(data, [], "dac", "out", set())
+    for node in visited:
+        if node in data:
+            data.pop(node)
+    print("done with dac to out")
+    print(result)
+    extra_result, visited = find_all_paths_visited(data, [], "fft", "dac", set())
+    for node in visited:
+        if node in data:
+            data.pop(node)
+    result *= extra_result
+    print("done with fft to dac")
+    print(result)
+    extra_result, visited = find_all_paths_visited(data, [], "svr", "fft", set())
+    result *= extra_result
+    print("done with svr to fft")
     return result
 
 def solve(puzzle_input):
@@ -24,7 +68,7 @@ def solve(puzzle_input):
     sol1 = part1(data)
     data = parse(puzzle_input)
     sol2 = part2(data)
-    return sol1, sol2
+    return sol2
 
 
 def run():
